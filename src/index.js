cee5f592b29eacf28d5861120fb9245984d2f649
CORP\gabrun <gabriel.brun@capgemini.com>
2017-05-23 11:17:18 +0200

    The markdown editor is working

--- src/index.js
+++ src/index.js
@@ -1,1 +1,59 @@
-// Ici on code notre App
+import React from 'react';
+import { render } from 'react-dom';
+//CSS
+import './style/css/bootstrap.min.css';
+import './index.css';
+//JS PERSO
+import {sampleText} from './sampleText.js';
+// Marked.js
+import marked from 'marked';
+
+class App extends React.Component{
+    state = {
+        text: sampleText
+    }
+    componentWillMount(){ 
+        // quand le composent se charge. Il se monte 1 foit puis quand on quitte se démonte. Entre les 2 c'est des updates.
+        const localStorageText = localStorage.getItem('text');
+        console.log(localStorageText);
+        if(localStorageText){
+            this.setState({text:localStorageText});
+        }
+    }
+    componentWillUpdate(nextProps, nextState){ // à la maj du state
+        localStorage.setItem('text', nextState.text);
+    }
+    editText = (event) => {
+        const text = event.target.value;
+        this.setState({text});
+    }
+    renderText = (text) => {
+        const renderText = marked(text, {sanitize: true});
+        return {__html: renderText};
+    }
+    render(){
+        return(
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6">
+                        <textarea 
+                            className="form-control" 
+                            rows="35" 
+                            value={this.state.text}
+                            onChange={(e) => this.editText(e)}
+                        >
+                        
+                        </textarea>
+                    </div>
+                    <div className="col-sm-6">
+                        <div dangerouslySetInnerHTML={this.renderText(this.state.text)} />
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+render(
+    <App />,
+    document.getElementById('root')
+);
